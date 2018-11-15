ff0c896e94bf06d44d8f5a3c25b9e5d5b4ea68fd
Norbiox <norberto9411@gmail.com>
2018-11-15 17:09:31 +0100

    created basic swagger API

--- labapp/app/api/lab.py
+++
@@ -1,4 +0,0 @@
-import flask
-
-def index():
-	return []
--- labapp/app/config.py
+++ labapp/app/config.py
@@ -1,4 +1,14 @@
+import os
+
 from dotenv import load_dotenv
 load_dotenv()
 
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST', default='172.17.0.2')
+DB_PORT = os.getenv('DB_PORT', default='3306')
+
 DEBUG=True
+SQLALCHEMY_DATABASE_URI="mysql://{}:{}@{}:{}/mysql".format(
+	DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
+)
