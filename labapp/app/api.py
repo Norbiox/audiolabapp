379a5e3b4b28f9e3960bf68df0f3793538db1c2a
Norbiox <norberto9411@gmail.com>
2018-11-21 12:04:40 +0100

    migrated api specification to OpenAPI 3.0.0 version

--- labapp/app/api.py
+++ labapp/app/api.py
@@ -3,9 +3,67 @@ import flask
 from .models import Recorder
 
 
-def index():
-	return []
-
-
 def get_recorders():
 	return []
+
+
+def new_recorder():
+	return {}
+
+
+def get_recorder():
+	return {}
+
+
+def update_recorder():
+	return {}
+
+
+def delete_recorder():
+	return {}
+
+
+def get_serieses():
+	return []
+
+
+def new_series():
+	return {}
+
+
+def get_series():
+	return {}
+
+
+def update_series():
+	return {}
+
+
+def delete_series():
+	return {}
+
+
+def get_records():
+	return []
+
+
+def new_record():
+	return {}
+
+
+def get_record():
+	return {}
+
+
+def update_record():
+	return {}
+
+
+def delete_record():
+	return {}
+
+
+def upload_record():
+	return []
+
+
--- labapp/app/models.py
+++ labapp/app/models.py
@@ -2,10 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Recorder(db.Model):
 	__tablename__ = 'recorder'
 
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
 	uid = db.Column(db.String(5), index=True)
 	location_description = db.Column(db.String(100), nullable=True)
 
@@ -14,3 +16,17 @@ class Recorder(db.Model):
 			'uid' : self.uid,
 			'location_description': self.location_description
 		}
+
+
+class Series(db.Model):
+	__tablename__ = 'series'
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	created_at = db.Column(db.DateTime, default=db.func.now())
+
+	uid = db.Column(db.String(5), index=True)
+	min_duration = db.Column(db.Float)
+	max_duration = db.Column(db.Float)
+
+
+c
